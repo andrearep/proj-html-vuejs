f7dfda61a1422d0e4c94cf0223967880bf4d40a8
andrearep <andrea.repe@gmail.com>
2021-06-17 11:10:35 +0200

    aggiunta mappa

--- dist/js/app.js
+++ dist/js/app.js
@@ -7,18 +7,6 @@
   \********************/
 /***/ (() => {
 
-var map;
-
-function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: {
-      lat: -34.397,
-      lng: 150.644
-    },
-    zoom: 8
-  });
-}
-
 var app = new Vue({
   el: '#app',
   data: {
